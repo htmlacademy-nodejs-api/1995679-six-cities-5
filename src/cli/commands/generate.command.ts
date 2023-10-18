a62a1956735903afad570aac4d5815933796b953
Alexey Demyanov <deal110988@him-mail.ch>
2023-10-18 17:02:07 +0200

    to test createOffer function

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -2,9 +2,20 @@ import got from "got";
 import { appendFile } from "node:fs/promises";
 import { Command } from "./command.interface.js";
 import { MockServerData } from "../../shared/types/index.js";
+import { TSVOfferGenerator } from "../../shared/libs/offer-generator/tsv-offer-generator.js";
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
+
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
+        encoding: "utf-8",
+      });
+    }
+  }
 
   private async load(url: string) {
     try {
@@ -24,6 +35,8 @@ export class GenerateCommand implements Command {
 
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} created`);
     } catch (error: unknown) {
       console.error(`Failed to generate data`);
 
