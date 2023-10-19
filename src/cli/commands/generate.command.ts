0e551764470c7956b1ab22a782839637f04d2bd7
Alexey Demyanov <deal110988@him-mail.ch>
2023-10-19 16:58:19 +0200

    finished 2.1

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,19 +1,19 @@
 import got from "got";
-import { appendFile } from "node:fs/promises";
 import { Command } from "./command.interface.js";
 import { MockServerData } from "../../shared/types/index.js";
-import { TSVOfferGenerator } from "../../shared/libs/offer-generator/tsv-offer-generator.js";
+import { TSVOfferGenerator } from "../../shared/libs/offer-generator/index.js";
+import { getErrorMessage } from "../../shared/helpers/index.js";
+import { TSVFileWriter } from "../../shared/libs/file-writer/index.js";
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
 
   private async write(filepath: string, offerCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
 
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
-        encoding: "utf-8",
-      });
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
@@ -39,10 +39,7 @@ export class GenerateCommand implements Command {
       console.info(`File ${filepath} created`);
     } catch (error: unknown) {
       console.error(`Failed to generate data`);
-
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
+      console.error(getErrorMessage(error));
     }
   }
 }
--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,76 +1,36 @@
 import { FileReader } from "./file-reader.interface.js";
-import { readFileSync } from "node:fs";
-import { Offer, OfferType, OfferCity } from "../../types/index.js";
+import EventEmitter from "node:events";
+import { createReadStream } from "node:fs";
 
-export class TSVFileReader implements FileReader {
-  private rawData = "";
+const CHUNK_SIZE = 16384;
 
-  constructor(private readonly filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: "utf-8" });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error(`File was not read`);
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: "utf-8",
+    });
+
+    let remainingData = "";
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf("\n")) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit("line", completeRow);
+      }
     }
 
-    return this.rawData
-      .split("\n")
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split("\t"))
-      .map(
-        ([
-          title,
-          description,
-          postDate,
-          city,
-          imagePreview,
-          accommodationPhotos,
-          isPremium,
-          isFavorite,
-          rating,
-          type,
-          rooms,
-          guests,
-          price,
-          facilities,
-          author,
-          comments,
-          coordinates,
-        ]) => ({
-          title,
-          description,
-          postDate: new Date(postDate),
-          city: OfferCity[
-            city as
-              | "Paris"
-              | "Cologne"
-              | "Brussels"
-              | "Amsterdam"
-              | "Hamburg"
-              | "Dusseldorf"
-          ],
-          imagePreview,
-          accommodationPhotos: accommodationPhotos
-            .split(";")
-            .map((photo) => photo),
-          isPremium,
-          isFavorite,
-          rating: Number.parseInt(rating, 10),
-          type: OfferType[type as "Apartment" | "House" | "Room" | "Hotel"],
-          rooms: Number.parseInt(rooms, 10),
-          guests: Number.parseInt(guests, 10),
-          price: Number.parseInt(price, 10),
-          facilities: facilities.split(";").map((facility) => facility),
-          author,
-          comments: Number.parseInt(comments, 10),
-          coordinates: {
-            latitude: coordinates.split(';')[0],
-            longitude: coordinates.split(';')[1],
-          },
-        })
-      );
+    this.emit("end", importedRowCount);
   }
 }
